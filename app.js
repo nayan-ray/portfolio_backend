b720b681d71ce442018709d873a507b76e590731
Nayan <niranzanroy1@gmail.com>
2026-07-07 19:39:38 +0600

    max -size express json

--- app.js
+++ app.js
@@ -17,7 +17,22 @@ import authRoute from "./src/route/authRoute.js"
 
 const app = express();
 
-app.use(express.json());
+
+app.use(
+  express.json({
+    limit: "5mb",
+  })
+);
+
+// Parse Form Data
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "5mb",
+  })
+);
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
